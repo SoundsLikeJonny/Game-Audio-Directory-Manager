c015e7b4b538b0fa6a524c237acfc8c3cb254da4
SoundsLikeJonny <jonnythejet@gmail.com>
2020-10-05 09:52:12 -0700

    Intergrated Node.js, Electron.js, npm python-shell

--- dir.py
+++ dir.py
@@ -1,6 +1,7 @@
 import os
 import yaml
 import pathlib
+import shutil
 
 class Dir():
     """
@@ -18,51 +19,52 @@ class Dir():
         if os.path.exists(self.dir_path):
             for index, data in enumerate(os.walk(self.dir_path)):
                 file_list[index] = {'dirpath': data[0], 'dirnames': data[1], 'filenames': data[2]}
+                
         else:
             print('Directory not found')
 
         return file_list
 
-    def get_filenames(self, dir_):
+
+    def get_filenames(self, dir_) -> list:
         """
         """
         files_list = []
         for dir_index in range(len(dir_)):
             files_list += dir_[dir_index]['filenames']
-            
+
         return [file for file in files_list if file[-4:] == '.wav']
 
 
-    def create_dir(self, file_list: list, dir_destination: str) -> None:
+    def create_dir(self, file_list: list, dir_: str, dir_destination: str) -> None:
         """Creates a file directory according to the file names
         """
         for file in file_list:
-            if file[-4:] != '.wav':
-                filename = file.split('_')
-                print(filename)
-                path = dir_destination
+            filename = file.split('_')
+            path = dir_destination
 
             for folder in filename:
-                path += f'{folder}/'
-                print(path)
-            
+
+                if folder[-4:] != '.wav':
+                    path += f'{folder}/'
+
+            #if not os.path.exists(path):
+            os.makedirs(path, exist_ok=True)
+            shutil.copy(dir_[0]['dirpath']+file, path)
+            print(path)
+        
 
 
 
 # TODO : 
 if __name__ == "__main__":
 
-    protools_session_folder = '/Users/redfm/PycharmProjects/WwiseUE_Dir_Manager/Metal_Blades'
+    protools_session_folder = '/Users/jonevans/PycharmProjects/WwiseUE_Dir_Manager/Metal_Blades/'
 
-    dest_path = '/Users/redfm/PycharmProjects/WwiseUE_Dir_Manager/New'
+    dest_path = '/Users/jonevans/PycharmProjects/WwiseUE_Dir_Manager/New/'
 
     dir_ = Dir(protools_session_folder)
-
     directory = dir_.get_dir()
-
     file_names = dir_.get_filenames(directory)
-
-    dir_.create_dir(file_names, dest_path)
-    
-    
-
+    print(file_names)
+    dir_.create_dir(file_names, directory, dest_path)
