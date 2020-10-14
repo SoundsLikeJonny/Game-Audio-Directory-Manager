bf786f715045325feb312c16d220f62ccf54a0e3
Redfm <redfm@Redfms-iMac.local>
2020-10-13 17:31:35 -0700

    Tested wwise

--- wwise.py
+++ wwise.py
@@ -1,22 +1,49 @@
 from waapi import WaapiClient, CannotConnectToWaapiException
-
 from pprint import pprint
 
-
-try:
-
-    client = WaapiClient(allow_exception=True)
-
-    result = client.call("ak.wwise.core.getInfo")
-
-    print(result)
-
-    client.disconnect()
+from pathlib import PureWindowsPath
 
 
-except CannotConnectToWaapiException:
+def main():
+        
+    try:
 
-    print("Could not connect to Waapi: Is Wwise running and Wwise Authoring API enabled?")
+        client = WaapiClient()
+
+        args = {
+
+        "importOperation": "useExisting", 
+        "default": {
+            "importLanguage": "SFX"
+        }, 
+        "imports": [
+            {
+                "audioFile": "/Users/redfm/PycharmProjects/WwiseUE_Dir_Manager/New/Metal/Blades/Impact/Metal_Blades_Impact_01.wav", 
+                "objectPath": "\\Actor-Mixer Hierarchy\\Default Work Unit\\<Sequence Container>Test 0\\<Sound SFX>My SFX 0"
+            } 
+        ],
+        "return": [
+            "id", 
+            "name", 
+            "path"
+        ]
+    }
+
+        result = client.call("ak.wwise.core.getInfo")
+
+        print(result)
+
+        client.disconnect()
 
 
+    except CannotConnectToWaapiException:
+
+        print("Could not connect to Waapi: Is Wwise running and Wwise Authoring API enabled?")
+
+audioFile = "/Users/redfm/PycharmProjects/WwiseUE_Dir_Manager/New/Metal/Blades/Impact/Metal_Blades_Impact_01.wav"
+
+win_path = PureWindowsPath(audioFile)
+win_path = win_path.r#.split(r'\\')
+
+print(win_path)
 
